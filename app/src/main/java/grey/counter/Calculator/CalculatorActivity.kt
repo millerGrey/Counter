c6940abfa755d4e134390b5a967a2d5be52e4fff
Sergey <miller_serg@yahoo.com>
2019-12-22 23:03:54 +0300

    Добавил новый экран для рассчетов

---
+++ app/src/main/java/grey/counter/Calculator/CalculatorActivity.kt
@@ -0,0 +1,7 @@
+package grey.counter.Calculator
+
+import androidx.appcompat.app.AppCompatActivity
+
+class CalculatorActivity: AppCompatActivity() {
+
+}
---
+++ app/src/main/java/grey/counter/Calculator/CalculatorFragment.kt
@@ -0,0 +1,6 @@
+package grey.counter.Calculator
+
+import androidx.fragment.app.Fragment
+
+class CalculatorFragment: Fragment() {
+}
